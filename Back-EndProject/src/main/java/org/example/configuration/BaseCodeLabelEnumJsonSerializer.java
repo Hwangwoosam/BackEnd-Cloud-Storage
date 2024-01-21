59751597f6336f234de2a02e9bfa39469706c5f3
Hwangwoosam <sam1783@naver.com>
2024-01-21 16:06:09 +0900

    BaseCodeLabel Enum Json 객채 생성

---
+++ Back-EndProject/src/main/java/org/example/configuration/BaseCodeLabelEnumJsonSerializer.java
@@ -0,0 +1,21 @@
+package org.example.configuration;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.example.mvc.domain.BaseCodeLabelEnum;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseCodeLabelEnumJsonSerializer extends JsonSerializer<BaseCodeLabelEnum> {
+
+    @Override
+    public void serialize(BaseCodeLabelEnum value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        Map<String,Object> map = new HashMap<>();
+        map.put("code",value.code());
+        map.put("label",value.label());
+        gen.writeObject(map);
+    }
+}
