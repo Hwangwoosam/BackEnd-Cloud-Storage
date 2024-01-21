99db65a037a6bde22dd6921002a925815116e18a
Hwangwoosam <sam1783@naver.com>
2024-01-21 15:45:53 +0900

    step1 : 파일 업로드 기능 관련 DTO 생성

---
+++ Back-EndProject/src/main/java/org/example/mvc/domain/dto/UploadFileDTO.java
@@ -0,0 +1,38 @@
+package org.example.mvc.domain.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Data
+@Getter
+@Setter
+public class UploadFileDTO {
+    private int fileSeq;
+    private String userName;
+    private boolean thumbnailCheck;
+    private int fileSize;
+    private String fileName;
+    private String filePath;
+    private String originalPath;
+    private Date createTime;
+
+    public UploadFileDTO(){
+
+    }
+
+    public UploadFileDTO(int fileSeq,String userName, boolean thumbnailCheck
+                            , int size, String fileName, String filePath,
+                         String originalPath, Date createTime){
+        this.fileSeq = fileSeq;
+        this.userName = userName;
+        this.thumbnailCheck = thumbnailCheck;
+        this.fileSize = size;
+        this.fileName = fileName;
+        this.filePath = filePath;
+        this.originalPath = originalPath;
+        this.createTime = createTime;
+    }
+}
