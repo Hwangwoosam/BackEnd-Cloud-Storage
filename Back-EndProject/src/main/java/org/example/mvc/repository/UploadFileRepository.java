660983f4f5a6c736198868ea6784aa89afc6689c
Hwangwoosam <sam1783@naver.com>
2024-01-25 14:40:04 +0900

    step1: SQL 쿼리 추가 1.delete() 쿼리 추가

--- Back-EndProject/src/main/java/org/example/mvc/repository/UploadFileRepository.java
+++ Back-EndProject/src/main/java/org/example/mvc/repository/UploadFileRepository.java
@@ -1,17 +1,22 @@
 package org.example.mvc.repository;
 
-import org.example.mvc.domain.dto.MetadataInsertDTO;
+import org.example.mvc.domain.dto.MetaDataDTO;
 import org.example.mvc.domain.dto.UploadFileDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Repository
 public interface UploadFileRepository {
 
-    void save(MetadataInsertDTO uploadFileParameter);
-
-    List<UploadFileDTO> getUploadFileList();
-
+    void getFileInfo(MultipartFile multipartFile,String userName);
+    void save(UploadFileDTO uploadFileParameter);
+    List<MetaDataDTO> getList();
+    MetaDataDTO get(int fileSeq);
     void delete(int fileSeq);
+
+    ResponseEntity<byte[]> downloadFile(int fileSeq);
+
 }
