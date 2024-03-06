65bc43060e6c9b59f33206a818147f1defbc8a5b
Hwangwoosam <sam1783@naver.com>
2024-03-06 13:39:47 +0900

    step3

--- Back-EndProject/src/main/java/org/example/AssignmentApplication.java
+++ Back-EndProject/src/main/java/org/example/AssignmentApplication.java
@@ -2,10 +2,12 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class AssignmentApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AssignmentApplication.class, args);
     }
