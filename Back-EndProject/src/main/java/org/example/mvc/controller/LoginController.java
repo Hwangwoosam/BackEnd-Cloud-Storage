ab28202781143ed1d1f7cbcb6d22a90ee5ffe4c3
Hwangwoosam <sam1783@naver.com>
2024-03-06 13:41:26 +0900

    refact: login 기능 분리

---
+++ Back-EndProject/src/main/java/org/example/mvc/controller/LoginController.java
@@ -0,0 +1,59 @@
+package org.example.mvc.controller;
+
+import org.example.configuration.GlobalConfig;
+import org.example.configuration.exception.BaseException;
+import org.example.configuration.http.BaseResponseCode;
+import org.example.mvc.domain.dto.UploadUserDTO;
+import org.example.mvc.domain.dto.UserInfoDTO;
+import org.example.mvc.service.UploadFileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/user")
+public class LoginController {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+    @Autowired
+    private GlobalConfig globalConfig;
+    @Autowired
+    private UploadFileService uploadFileService;
+
+    @GetMapping("/login")
+    public String login(@RequestParam("userName") String userName){
+        logger.debug("login: {}",userName);
+        if(userName.isEmpty()){
+            throw new BaseException(BaseResponseCode.DATA_IS_NULL, new String[]{"사용자 이름"});
+        }
+
+        UserInfoDTO userInfoDTO = uploadFileService.getUser(userName);
+        if(userInfoDTO == null){
+            String uploadPath = globalConfig.getUploadFilePath();
+            String folderPath = UUID.randomUUID().toString();
+            File folder = new File(uploadPath + folderPath);
+            if(!folder.mkdir()){
+                throw new BaseException(BaseResponseCode.ERROR);
+            }
+            uploadFileService.setUser(new UploadUserDTO(userName,uploadPath + folderPath));
+            userInfoDTO = uploadFileService.getUser(userName);
+        }
+        String encodedName;
+        try {
+            encodedName = URLEncoder.encode(userInfoDTO.getUserName(),"UTF-8");
+        }catch (UnsupportedEncodingException e){
+            throw new RuntimeException("Error encoding userName", e);
+        }
+
+        return "redirect:/file/getList?userName="+encodedName + "&includeDir=0";
+    }
+}
