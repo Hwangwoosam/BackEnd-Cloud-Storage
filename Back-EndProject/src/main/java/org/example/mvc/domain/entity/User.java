b3444c57563ec7ec58b06fe27dbc3a045d71a3ea
Hwangwoosam <sam1783@naver.com>
2024-06-21 23:54:39 +0900

    implement: 유저 Entity 정의

--- Back-EndProject/src/main/java/org/example/mvc/domain/entity/User.java
+++ Back-EndProject/src/main/java/org/example/mvc/domain/entity/User.java
@@ -4,6 +4,6 @@ package org.example.mvc.domain.entity;
 public class User {
     private int id;
     private String userId;
-    private String folderName;
+    private String password;
     private String userName;
 }
