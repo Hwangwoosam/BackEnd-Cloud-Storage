4ae6320ca8ee572c38d591372e4c8e2fb06f4d20
Hwangwoosam <sam1783@naver.com>
2024-07-02 21:51:03 +0900

    implement: login 관련 기능 컨트롤러 추가 1. 아이디 중복 검사 2. 회원가입 기능 - Parameter 넘기는 과정에서 오류

--- Back-EndProject/src/main/java/org/example/mvc/controller/loginController.java
+++ Back-EndProject/src/main/java/org/example/mvc/controller/loginController.java
@@ -1,31 +1,72 @@
 package org.example.mvc.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.example.mvc.domain.dto.UserLoginDTO;
+import org.example.mvc.domain.dto.UserRegisterDTO;
+import org.example.mvc.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 @SpringBootApplication
 @Controller
 public class loginController {
 
-//    @GetMapping("login")
-//    public String login(){
-//
-//    }
-    @GetMapping("register")
-    public String home(){
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("login")
+    public String login(UserLoginDTO userLoginDto, HttpServletRequest request, RedirectAttributes rttr){
+
+
+        return "file_list.html";
+    }
+    @PostMapping("checkDuplicateId")
+    @ResponseBody
+    public boolean checkDuplicatedId(Map<String, String> payload){
+        String userId = payload.get("userId");
+        System.out.println(userId);
+        boolean isDuplicate = userService.checkDuplicateId(userId);
+        return  isDuplicate;
+    }
+
+    @PostMapping("register")
+    @ResponseBody
+    public Map<String,Object> register(UserRegisterDTO user){
+        Map<String,Object> response = new HashMap<>();
+        System.out.println(user.getUserId());
+        System.out.println(user.getUserName());
+        System.out.println(user.getPassword());
+        System.out.println(user.getEmail());
+        try {
+            userService.registerUser(user);
+            response.put("success",true);
+        }catch (Exception e){
+            response.put("success",false);
+            response.put("message",e.getMessage());
+        }
+
+        return response;
+    }
+
+    @GetMapping("registerPage")
+    public String registerPage(){
         return "register.html";
     }
 
-    @GetMapping("findId")
-    public String findId(){
+    @GetMapping("findIdPage")
+    public String findIdPage(){
         return "findId.html";
     }
 
-    @GetMapping("findPassword")
-    public String findPassword(){
+    @GetMapping("findPasswordPage")
+    public String findPasswordPage(){
         return "findPassword.html";
     }
 }
