7b4f4a29f101a8de4a29d7ae278f4c1fafff00fb
Hwangwoosam <sam1783@naver.com>
2024-07-02 21:43:34 +0900

    implement: DTO 별 생성자 추가

--- Back-EndProject/src/main/java/org/example/mvc/domain/entity/User.java
+++ Back-EndProject/src/main/java/org/example/mvc/domain/entity/User.java
@@ -1,6 +1,9 @@
 package org.example.mvc.domain.entity;
 
 
+import org.example.mvc.domain.dto.UserLoginDTO;
+import org.example.mvc.domain.dto.UserRegisterDTO;
+
 public class User {
     private int id;
     private String userId;
@@ -9,4 +12,18 @@ public class User {
     private String phoneNumber;
     private String email;
     private Long totalSize;
+
+    public User(UserRegisterDTO user){
+        this.userId = user.getUserId();
+        this.password = user.getPassword();
+        this.userName = user.getUserName();
+        this.phoneNumber = user.getPhoneNumber();
+        this.email = user.getEmail();
+        this.totalSize = 0L;
+    }
+
+    public User(UserLoginDTO user){
+        this.userId = user.getUserId();
+        this.password = user.getPassword();
+    }
 }
