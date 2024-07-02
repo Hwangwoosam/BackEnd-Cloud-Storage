41c99fa0857c031af14fbf5fc1e5957bf0efbef4
Hwangwoosam <sam1783@naver.com>
2024-07-02 21:45:11 +0900

    implement: 회원가입 DTO 추가

--- Back-EndProject/src/main/java/org/example/mvc/domain/dto/UserRegisterDTO.java
+++ Back-EndProject/src/main/java/org/example/mvc/domain/dto/UserRegisterDTO.java
@@ -1,19 +1,28 @@
 package org.example.mvc.domain.dto;
 
-public class UserDTO {
-    private int id;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.mvc.domain.entity.User;
+
+@Getter
+@Setter
+public class UserRegisterDTO {
     private String userId;
     private String password;
     private String userName;
     private String phoneNumber;
     private String email;
-    private Long totalSize;
 
-    public UserDTO(String userId, String password, String userName, String phoneNumber, String email){
+    public UserRegisterDTO(){
+
+    }
+
+    public UserRegisterDTO(String userId, String password, String userName, String phoneNumber, String email){
         this.userId = userId;
         this.password = password;
         this.userName = userName;
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
+
 }
