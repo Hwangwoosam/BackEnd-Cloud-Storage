ca7aaddb29cb6f41f431235effa84007479ef6fd
Hwangwoosam <sam1783@naver.com>
2024-07-06 21:50:55 +0900

    implement: 나머지 정보에 대한 유효성 검사 프론트 부분 추가 이메일 인증은 추후에 추가 예정

--- Back-EndProject/src/main/resources/static/js/register.js
+++ Back-EndProject/src/main/resources/static/js/register.js
@@ -7,6 +7,13 @@ let elInputPasswordRetype = document.querySelector('#password-retype');
 let elMismatchMessage = document.querySelector('.mismatch-message');
 let elStrongPasswordMessage = document.querySelector('.strongPassword-message');
 
+let elInputUserName = document.getElementById("userName");
+let elUserNameFailureMessage = document.querySelector('.name-failure-message');
+let elUserNameFailureMessage2 = document.querySelector('.name-failure-message2');
+
+let elInputPhoneNumber = document.getElementById("phoneNumber");
+let elPhoneNumberFailureMessage = document.querySelector('.phoneNumber-failure-message');
+
 document.addEventListener('DOMContentLoaded',function(){
     const checkDuplicateButton = document.getElementById('checkDuplicate');
     checkDuplicateButton.addEventListener('click',checkDuplicateId);
@@ -22,8 +29,16 @@ function idLength(value){
     return value.length >= 4 && value.length <= 12
 }
 
+function nameLength(value){
+    return value.length >= 2 && value.length <= 10
+}
+
 function onlyNumberAndEnglish(str){
     return /^[A-Za-z0-9]+$/.test(str);
+}
+
+function onlyKoreanAndEnglish(str){
+    return /^[가-힣a-zA-Z]+$/.test(str);
 }
 
 function strongPassword(str){
@@ -33,6 +48,11 @@ function strongPassword(str){
 function isMatch (password1, password2) {
   return password1 === password2;
 }
+
+function checkPhoneNumber(str){
+    return /^(0\d{1,3})-(\d{3,4})-(\d{4})$/.test(str);
+}
+
 
 elInputUserId.onkeyup = function(){
     if (elInputUserId.value.length !== 0) {
@@ -76,6 +96,38 @@ elInputPasswordRetype.onkeyup = function(){
     }
 }
 
+elInputUserName.onkeyup = function(){
+    if(elInputUserName.value.length !== 0){
+        if(onlyKoreanAndEnglish(elInputUserName.value) === false){
+            elUserNameFailureMessage.classList.remove('hide');
+            elUserNameFailureMessage2.classList.add('hide');
+        }
+        else if(nameLength(elInputUserName.value) === false){
+            elUserNameFailureMessage.classList.add('hide');
+            elUserNameFailureMessage2.classList.remove('hide');
+        }else{
+            elUserNameFailureMessage.classList.add('hide');
+            elUserNameFailureMessage2.classList.add('hide');
+        }
+    }else{
+        elUserNameFailureMessage.classList.add('hide');
+        elUserNameFailureMessage2.classList.add('hide');
+    }
+}
+
+elInputPhoneNumber.onkeyup = function(){
+    if(elInputPhoneNumber.value.length !== 0){
+        if(checkPhoneNumber(elInputPhoneNumber.value) === false){
+            elPhoneNumberFailureMessage.classList.remove('hide');
+        }else{
+            elPhoneNumberFailureMessage.classList.add('hide');
+        }
+    }else{
+        elPhoneNumberFailureMessage.classList.add('hide');
+    }
+}
+
+
 function checkDuplicateId(){
 
     const id = document.getElementById('userId').value;
@@ -115,7 +167,7 @@ function submitForm() {
     fetch('/register', {
         method: 'POST',
         headers: {
-               "Content-Type":"application/json; charset=utf-8"
+               "Content-Type":"application/json;"
         },
         body: JSON.stringify(userData)
     })
