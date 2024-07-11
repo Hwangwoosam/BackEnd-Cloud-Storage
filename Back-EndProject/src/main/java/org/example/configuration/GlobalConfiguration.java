5875b2a0b9a20c766c99312abd4d8c07fa0291d0
Hwangwoosam <sam1783@naver.com>
2024-07-11 19:03:58 +0900

    implement: 업로드 파일 경로 설정

---
+++ Back-EndProject/src/main/java/org/example/configuration/GlobalConfiguration.java
@@ -0,0 +1,18 @@
+package org.example.configuration;
+
+import jakarta.validation.Valid;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class GlobalConfiguration {
+
+    @Getter
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @Value("${uploadFile.resourcePath}")
+    private String uploadFilePath;
+}
