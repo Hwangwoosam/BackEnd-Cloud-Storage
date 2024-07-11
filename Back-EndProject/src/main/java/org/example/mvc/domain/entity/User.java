5643165a0d0130414024cb89ee145471c69bca53
Hwangwoosam <sam1783@naver.com>
2024-07-11 19:07:03 +0900

    implement: User별 디렉토리 경로 추가

--- Back-EndProject/src/main/java/org/example/mvc/domain/entity/User.java
+++ Back-EndProject/src/main/java/org/example/mvc/domain/entity/User.java
@@ -12,14 +12,16 @@ public class User {
     private String phoneNumber;
     private String email;
     private Long totalSize;
+    private String rootPath;
 
-    public User(UserRegisterDTO user){
+    public User(UserRegisterDTO user, String rootPath){
         this.userId = user.getUserId();
         this.password = user.getPassword();
         this.userName = user.getUserName();
         this.phoneNumber = user.getPhoneNumber();
         this.email = user.getEmail();
         this.totalSize = 0L;
+        this.rootPath = rootPath;
     }
 
     public User(UserLoginDTO user){
