2ede50890ffef3766e32f18f1cca6a7efa951c41
Hwangwoosam <sam1783@naver.com>
2024-07-21 00:26:16 +0900

    implement: 비밀번호 및 아이디 찾기 관련 기능 추가

--- Back-EndProject/src/main/java/org/example/mvc/repository/UserRepository.java
+++ Back-EndProject/src/main/java/org/example/mvc/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package org.example.mvc.repository;
 
+import org.example.mvc.domain.dto.UserChangePassword;
+import org.example.mvc.domain.dto.UserFindIdDTO;
+import org.example.mvc.domain.dto.UserFindPasswordDTO;
 import org.example.mvc.domain.dto.UserLoginDTO;
 import org.example.mvc.domain.entity.User;
 import org.springframework.stereotype.Repository;
@@ -12,4 +15,9 @@ public interface UserRepository {
 
     int isRegister(UserLoginDTO userLoginDTO);
 
+    String findUserId(UserFindIdDTO userFindIdDTO);
+
+    int findId(UserFindPasswordDTO userFindPasswordDTO);
+
+    int changePassword(UserChangePassword userChangePassword);
 }
