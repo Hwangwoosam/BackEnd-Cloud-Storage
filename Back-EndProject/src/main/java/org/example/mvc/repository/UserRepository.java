8f664beafc4d7d05b1756f1321fa8d6ec81df3fc
Hwangwoosam <sam1783@naver.com>
2024-08-11 21:31:51 +0900

    refactoring: Spring Security 적용을 위한 코드 수정

--- Back-EndProject/src/main/java/org/example/mvc/repository/UserRepository.java
+++ Back-EndProject/src/main/java/org/example/mvc/repository/UserRepository.java
@@ -1,23 +1,14 @@
 package org.example.mvc.repository;
 
-import org.example.mvc.domain.dto.UserChangePassword;
-import org.example.mvc.domain.dto.UserFindIdDTO;
-import org.example.mvc.domain.dto.UserFindPasswordDTO;
-import org.example.mvc.domain.dto.UserLoginDTO;
+import org.example.mvc.domain.dto.User.*;
 import org.example.mvc.domain.entity.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository {
-
-    int checkDuplicateId(String userId);
     int register(User user);
-
-    int isRegister(UserLoginDTO userLoginDTO);
-
-    String findUserId(UserFindIdDTO userFindIdDTO);
-
-    int findId(UserFindPasswordDTO userFindPasswordDTO);
-
+    UserInfoDTO findByUserId(String userId);
+    UserInfoDTO findByUserName(String userName);
+//    UserInfoDTO login(UserLoginDTO userLoginDTO);
     int changePassword(UserChangePassword userChangePassword);
 }
--- Back-EndProject/src/main/java/org/example/mvc/service/UserService.java
+++ Back-EndProject/src/main/java/org/example/mvc/service/UserService.java
@@ -3,7 +3,7 @@ package org.example.mvc.service;
 //import org.example.configuration.GlobalConfig;
 //import org.example.mvc.domain.dto.UserInfoDTO;
 import org.example.configuration.GlobalConfiguration;
-import org.example.mvc.domain.dto.*;
+import org.example.mvc.domain.dto.User.*;
 import org.example.mvc.domain.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.example.mvc.repository.UserRepository;
@@ -18,16 +18,19 @@ public class UserService {
 
     @Autowired
     private GlobalConfiguration globalConfiguration;
+
     @Autowired
     private UserRepository userRepository;
 
-    public boolean checkDuplicateId(String id){
-        int cnt = userRepository.checkDuplicateId(id);
-        System.out.println(cnt);
-        if(cnt > 0) return true;
-        return false;
+    public UserInfoDTO findByUserId(String userId){
+        UserInfoDTO userInfoDTO = userRepository.findByUserId(userId);
+        return userInfoDTO;
     }
 
+    public UserInfoDTO findByName(String userName){
+        UserInfoDTO userInfoDTO = userRepository.findByUserName(userName);
+        return userInfoDTO;
+    }
     @Transactional
     public boolean registerUser(UserRegisterDTO userDto){
         String folderName = UUID.randomUUID().toString();
@@ -40,24 +43,22 @@ public class UserService {
             System.out.println("User Directory Created Success");
         }else{
             System.out.println("User Directory Created failed");
+            return false;
         }
 
         return true;
     }
 
+//    public UserInfoDTO login(UserLoginDTO userLoginDTO){
+//        return userRepository.login(userLoginDTO);
+//    }
+//
 
-    public boolean login(UserLoginDTO userLoginDTO){
-        return userRepository.isRegister(userLoginDTO) == 1;
-    }
-
-    public String findUserIdByNameAndEmail(UserFindIdDTO userFindIdDTO){
-        return userRepository.findUserId(userFindIdDTO);
-    }
-
-    public Integer findIdByUserIdAndEmail(UserFindPasswordDTO userFindPasswordDTO){
-        return userRepository.findId(userFindPasswordDTO);
-    }
-
+//
+//    public Integer findIdByUserIdAndEmail(UserFindPasswordDTO userFindPasswordDTO){
+//        return userRepository.findId(userFindPasswordDTO);
+//    }
+//
     public boolean changePassword(UserChangePassword userChangePassword){
         return userRepository.changePassword(userChangePassword) == 1;
     }
