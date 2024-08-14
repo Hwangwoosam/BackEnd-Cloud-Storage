43d6da94208a3e802303181a9f6494cb3ca08eba
Hwangwoosam <sam1783@naver.com>
2024-08-14 23:31:35 +0900

    refactoring: 사용하지 않는 DTO 제거

--- Back-EndProject/src/main/java/org/example/mvc/domain/entity/User.java
+++ Back-EndProject/src/main/java/org/example/mvc/domain/entity/User.java
@@ -1,31 +1,27 @@
 package org.example.mvc.domain.entity;
 
 
-import org.example.mvc.domain.dto.UserLoginDTO;
-import org.example.mvc.domain.dto.UserRegisterDTO;
+import lombok.Getter;
+import org.example.mvc.domain.dto.User.UserRegisterDTO;
 
+@Getter
 public class User {
     private int id;
     private String userId;
     private String password;
-    private String userName;
+    private String name;
     private String phoneNumber;
     private String email;
     private Long totalSize;
     private String rootPath;
 
-    public User(UserRegisterDTO user, String rootPath){
+    public User(UserRegisterDTO user,String encodedPassword ,String rootPath){
         this.userId = user.getUserId();
-        this.password = user.getPassword();
-        this.userName = user.getUserName();
+        this.password =  encodedPassword;
+        this.name = user.getUserName();
         this.phoneNumber = user.getPhoneNumber();
         this.email = user.getEmail();
         this.totalSize = 0L;
         this.rootPath = rootPath;
-    }
-
-    public User(UserLoginDTO user){
-        this.userId = user.getUserId();
-        this.password = user.getPassword();
     }
 }
