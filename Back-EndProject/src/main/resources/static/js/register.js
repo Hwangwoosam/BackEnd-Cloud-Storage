15a79c4b60faa775ae9dc2e18a587df2422ce34b
Hwangwoosam <sam1783@naver.com>
2024-08-14 22:19:52 +0900

    implement: ID 부분 입력 변화시 다시 중복체크하도록 수정

--- Back-EndProject/src/main/resources/static/js/register.js
+++ Back-EndProject/src/main/resources/static/js/register.js
@@ -221,7 +221,7 @@ function checkDuplicateId(){
          })
         .then(data => {
             console.log(data.isDuplicate);
-            if(!data.isDuplicate){
+            if(data.isDuplicate){
                 resultDiv.style.color = 'red';
                 resultDiv.textContent = '이미 존재하는 아이디입니다.';
                 isIdChecked = false;
