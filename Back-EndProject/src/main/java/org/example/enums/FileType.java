9381c3ec6d4e974a371a3500f14f7767df469aea
Hwangwoosam <sam1783@naver.com>
2024-09-29 22:48:49 +0900

    implement: 파일 타입을 직관적으로 보기 위한 enum 정의

--- Back-EndProject/src/main/java/org/example/enums/FileType.java
+++ Back-EndProject/src/main/java/org/example/enums/FileType.java
@@ -1,2 +1,26 @@
-package org.example.enums;public class FileType {
+package org.example.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum FileType {
+    FILE("FOLDER",0),
+    FOLDER("FILE",1);
+
+    private final String value;
+    private final Integer code;
+
+    FileType(String value, Integer code) {
+        this.value = value;
+        this.code = code;
+    }
+
+    public static FileType fromCode(int code) {
+        for (FileType type : values()) {
+            if (type.getCode() == code) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Invalid FileType code: " + code);
+    }
 }
